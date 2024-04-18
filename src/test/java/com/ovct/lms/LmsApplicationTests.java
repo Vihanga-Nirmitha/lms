3be88863e820d8f306eefbd839b9617710aab020
Tharindu Udara <tharindu@zerobeta.xyz>
2024-04-18 21:05:33 +0530

    first commit

---
+++ src/test/java/com/ovct/lms/LmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovct.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
